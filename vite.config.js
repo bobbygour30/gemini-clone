96ee9c29013673ced55a45dd5f228e3dc52e5202
bobbygour30 <bobby30gour@gmail.com>
2024-07-12 15:46:14 +0530

    vite.config updated

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@google/generative-ai': path.resolve(__dirname, 'node_modules/@google/generative-ai/dist/index.js')
-    }
-  },
-  optimizeDeps: {
-    include: ['@google/generative-ai'],
-  },
   build: {
     rollupOptions: {
       external: ['@google/generative-ai'],
     },
   },
+  plugins: [react()],
 })
